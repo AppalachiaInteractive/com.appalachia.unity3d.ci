e9ecade3c124e8f56195e144b8cbdf1fd8db8e5a
Chris Schubert <chris.a.schubert25@gmail.com>
2021-10-31 15:54:26 -0400

    Updating to 0.2 for packaging simplicity

--- src/SemVer/Tests/PKG.cs
+++ src/SemVer/Tests/PKG.cs
@@ -8,7 +8,7 @@ namespace Appalachia.CI.SemVer
 {
     internal static partial class PKG
     {
-        public const int Priority = -459000;
+        public const int Priority = -223000;
         public const string Name = "CI/SemVer";
         public const string Prefix = Root + Name + "/";
         public const string Root = "Appalachia/";
